8fca4fa498de4c69886c6f6e2552a07397afcc43
PlengNakdee <plengwn@gmail.com>
2021-04-03 15:47:45 +0000

    checkpoint

---
+++ whatsapp-2/components/Sidebar.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+function Sidebar() {
+    return(
+        <Container>
+            <Header>
+                This is a header
+            </Header>
+        </Container>
+    )
+}
+
+const Container = styled.div``;
+
+const Header = styled.div``;
