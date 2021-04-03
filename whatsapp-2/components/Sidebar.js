0f8b48641b4c89b33723e66eebda13d1e95ee9db
PlengNakdee <plengwn@gmail.com>
2021-04-03 16:07:23 +0000

    add icons

--- whatsapp-2/components/Sidebar.js
+++ whatsapp-2/components/Sidebar.js
@@ -1,15 +1,32 @@
 import styled from 'styled-components';
+import { Avatar, IconButton } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Sidebar() {
     return(
         <Container>
             <Header>
-                This is a header
+                <UserAvatar />
+                <IconsContainer>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </IconsContainer>
             </Header>
         </Container>
     )
 }
 
+export default Sidebar;
+
 const Container = styled.div``;
 
 const Header = styled.div``;
+
+const UserAvatar = styled(Avatar)``;
+
+const IconsContainer = styled.div``;
--- whatsapp-2/pages/index.js
+++ whatsapp-2/pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import Sidebar from '../components/Sidebar';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Whatsapp Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Sidebar></Sidebar>
+      <Sidebar />
     
 
     </div>
