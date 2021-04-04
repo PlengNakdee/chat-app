d99a51311d309742f9b304af195d50047df2ef5b
Pleng Mac <plengmac@Plengs-MacBook-Pro.local>
2021-04-04 21:19:35 +0700

    checkpoint

--- whatsapp-2/pages/_app.js
+++ whatsapp-2/pages/_app.js
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../firebase';
 
 function MyApp({ Component, pageProps }) {
+  const [user] = useAuthState(auth);
+
+  if (!user) return <Login />
+
   return <Component {...pageProps} />
 }
 
