3e2ceaeb84c13feb2aa4da858452c1e8d90b5603
Pleng Mac <plengmac@Plengs-MacBook-Pro.local>
2021-04-08 00:51:21 +0700

    add chat screen

--- whatsapp-2/components/ChatScreen.js
+++ whatsapp-2/components/ChatScreen.js
@@ -1,9 +1,32 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
+import { auth } from "../firebase";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
 
-function ChatScreen() {
+function ChatScreen({ chat, messages }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+
   return (
     <Container>
-      <h1>ChatScreen</h1>
+      <Header>
+        <Avatar />
+        <HeaaderInformation>
+          <h3>Rec email</h3>
+          <p>Last seen...</p>
+        </HeaaderInformation>
+        <HeaderIcon>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </HeaderIcon>
+      </Header>
     </Container>
   );
 }
@@ -11,3 +34,9 @@ function ChatScreen() {
 export default ChatScreen;
 
 const Container = styled.div``;
+
+const Header = styled.div``;
+
+const HeaaderInformation = styled.div``;
+
+const HeaderIcon = styled.div``;
--- whatsapp-2/pages/chat/[id].js
+++ whatsapp-2/pages/chat/[id].js
@@ -2,22 +2,59 @@ import styled from "styled-components";
 import Head from "next/head";
 import Sidebar from "../../components/Sidebar";
 import ChatScreen from "../../components/ChatScreen";
+import { auth, db } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import getRecipientEmail from "../../utils/getRecipientEmail";
 
-function Chat() {
+function Chat({ chat, messages }) {
+  const [user] = useAuthState(auth);
   return (
     <Container>
       <Head>
-        <title>Chat</title>
+        <title>Chat with {getRecipientEmail(chat.users, user)}</title>
       </Head>
       <Sidebar />
       <ChatContainer>
-        <ChatScreen />
+        <ChatScreen chat={chat} messages={messages} />
       </ChatContainer>
     </Container>
   );
 }
 
 export default Chat;
+
+export async function getServerSideProps(context) {
+  const ref = db.collection("chats").doc(context.query.id);
+  // prep messages on the server
+  const messagesRes = await ref
+    .collection("messages")
+    .orderBy("timestamp", "asc")
+    .get();
+
+  const messages = messagesRes.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+    .map((messages) => ({
+      ...messages,
+      timestamp: messages.timestamp.toDate().getTime(),
+    }));
+
+  // prep the chats
+  const chatRes = await ref.get();
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data(),
+  };
+
+  return {
+    props: {
+      messages: JSON.stringify(messages),
+      chat: chat,
+    },
+  };
+}
 
 const Container = styled.div`
   display: flex;
