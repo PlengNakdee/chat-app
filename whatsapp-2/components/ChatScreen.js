a032f68653d6fb2c916bbf8535e930cab01d88ed
Pleng Mac <plengmac@Plengs-MacBook-Pro.local>
2021-04-08 22:51:23 +0700

    checkpoint

--- whatsapp-2/components/ChatScreen.js
+++ whatsapp-2/components/ChatScreen.js
@@ -2,13 +2,37 @@ import { Avatar, IconButton } from "@material-ui/core";
 import { useRouter } from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
+import { useCollection } from "react-firebase-hooks/firestore";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function ChatScreen({ chat, messages }) {
   const [user] = useAuthState(auth);
   const router = useRouter();
+  const [messagesSnapshot] = useCollection(
+    db
+      .collection("chats")
+      .doc(router.query.id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+  );
+  const showMessages = () => {
+    if (messagesSnapshot) {
+      return messagesSnapshot.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.user}
+          messages={{
+            ...messages.data(),
+            timestamp: messages.timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
+    }
+  };
 
   return (
     <Container>
@@ -27,6 +51,15 @@ function ChatScreen({ chat, messages }) {
           </IconButton>
         </HeaderIcon>
       </Header>
+      <MessageContainer>
+        {showMessages()}
+        <EndOfMessage />
+      </MessageContainer>
+      <InputContainer>
+        <InsertEmoticonIcon />
+        <Input />
+        <MicIcon />
+      </InputContainer>
     </Container>
   );
 }
@@ -35,8 +68,53 @@ export default ChatScreen;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
 
-const HeaaderInformation = styled.div``;
+const HeaaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+
+  > h3 {
+    margin-bottom: 3px;
+  }
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
 
 const HeaderIcon = styled.div``;
+
+const MessageContainer = styled.div``;
+
+const EndOfMessage = styled.div``;
+
+const InputContainer = styled.form`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  bottom: 0;
+  background-color: #fff;
+  z-index: 100;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  outline: 0;
+  border: none;
+  border-radius: 10px;
+  padding: 20px;
+  margin-left: 15px;
+  margin-right: 15px;
+  background-color: whitesmoke;
+`;
